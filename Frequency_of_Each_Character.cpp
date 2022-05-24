860f576cc7764117bc45d7cdfa8b476f4e6f3724
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-05-24 22:12:37 +0530

    Create Frequency_of_Each_Character.cpp
    Here we consider that string consists only of lower case English alphabets. We take a vector of size 26, each representing count of occurrences of each character in English alphabets, and traverse through characters in the given string...
    Finally, we print the occurrence of each character...

---
+++ Frequency_of_Each_Character.cpp
@@ -0,0 +1,11 @@
+void frequency(string s) {
+    //Print Frequency of each character in a string in alphabatical order
+    vector<int> v(26, 0);
+    for(char c:s) v[c-'a']++;
+    
+    for(int i=0;i<26;i++){
+        if(v[i]!=0) {
+            cout << (char)(i+'a') << " " <<v[i] << endl;
+        }
+    }
+}
