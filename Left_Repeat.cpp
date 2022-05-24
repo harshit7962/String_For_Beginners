2357fd97585c6c487bdc2e4ddc8dfd82a66d6250
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-05-24 22:18:53 +0530

    Create Left_Repeat.cpp
    Here we need to print the leftmost character of a string that repeats itself...
    We do this by traversing the string in reverse order and keeping the track of visited characters in a set...

---
+++ Left_Repeat.cpp
@@ -0,0 +1,11 @@
+int leftMostRepeat(string s) {
+    //Index of the left most character which repeats itself
+    unordered_set<char> st;
+    int ind=-1;
+    for(int i=s.size()-1;i>=0;i--) {
+        if(st.count(s[i])==1) ind=i;
+        else st.insert(s[i]);
+    }
+
+    return ind;
+}
