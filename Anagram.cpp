7e31e5713fe37608073a5be834ddba0ba4055af0
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-05-24 22:17:40 +0530

    Create Anagram.cpp
    Here we need to check if strings s1 and s2 are anagrams of each other...
    This implies that if we arrange the alphabets of s2, we can get s1 and vice-versa...
    ex --->
    s1= ABCD
    s2= ADCB
    s3= BCDE
    s2 and s1 are anagram but s3 is not an anagram with any of s1 or s2.

---
+++ Anagram.cpp
@@ -0,0 +1,13 @@
+bool isAnagram(string s1, string s2) {
+    //Check if strings s1 and s2 are anagram of each other
+    if(s1.size()!=s2.size()) return false;
+    
+    vector<int> v(26,0);
+    for(char c:s1) v[c-'a']++;
+    for(char c:s2) v[c-'a']--;
+    
+    for(int i=0;i<26;i++)
+    if(v[i]!=0) return false;
+    
+    return true;
+}
