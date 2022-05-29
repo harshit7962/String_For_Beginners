cadf4227665f737e2e645de881662bf9effe1ff1
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-05-29 23:21:03 +0530

    Create reverse.cpp

---
+++ reverse.cpp
@@ -0,0 +1,9 @@
+//Reversing a string without stl
+void reverseString(vector<char>& s) {
+    int start=0, end=s.size()-1;
+    while(start<end) {
+        swap(s[start], s[end]);
+        start++;
+        end--;
+    }
+}
